b0d9fbb39136de2fe234422f9a6b53f9496167a5
visionsbm@gmail.com <visionsbm@gmail.com@234c8a7c-79c9-3d8c-7b9f-80a8cb237f45>
2011-12-05 09:32:10 +0000

    movement stuff

--- trunk/thiefworld/src/thiefworld/agents/Gatherer.java
+++ trunk/thiefworld/src/thiefworld/agents/Gatherer.java
@@ -19,6 +19,27 @@ public class Gatherer extends ActiveAgent {
 
 	@Override
 	public void step(SimState arg0) {
+		dropPheromone(arg0);
+		act(arg0);
+	}
+
+	private void act(SimState arg0) {
+		ThiefWorld world = (ThiefWorld) arg0;
+
+		// check if the agent is returning food
+		if (isReturningFood()) {
+			// follow the pheromone trail to the nest
+			
+		} else {
+			// follow the pheromone trail to a food source
+		}
+	}
+
+	private boolean isReturningFood() {
+		return this.getCarriedFood() == this.getMaxAllowedFood();
+	}
+
+	private void dropPheromone(SimState arg0) {
 		ThiefWorld world = (ThiefWorld) arg0;
 
 		// TODO check if a pheromone is not already in the same position. If so,
@@ -26,7 +47,7 @@ public class Gatherer extends ActiveAgent {
 		// value (i.e. unit).
 
 		// drop pheromone
-		Pheromone pheromone = new Pheromone(PheromoneType.Gatherer);
+		Pheromone pheromone = new Pheromone(PheromoneType.Hunter);
 		world.schedule.scheduleRepeating(pheromone);
 		world.map.setObjectLocation(pheromone,
 				world.map.getObjectLocation(this));
