843798401bcb417d1dcd98d171a8887110bb50d7
visionsbm@gmail.com <visionsbm@gmail.com@234c8a7c-79c9-3d8c-7b9f-80a8cb237f45>
2011-12-05 09:32:10 +0000

    movement stuff

--- thiefworld/src/thiefworld/agents/Hunter.java
+++ thiefworld/src/thiefworld/agents/Hunter.java
@@ -19,7 +19,20 @@ public class Hunter extends ActiveAgent {
 
 	@Override
 	public void step(SimState arg0) {
+		dropPheromone(arg0);
+		act(arg0);
+	}
+
+	private void act(SimState arg0) {
 		ThiefWorld world = (ThiefWorld) arg0;
+	}
+
+	private void dropPheromone(SimState arg0) {
+		ThiefWorld world = (ThiefWorld) arg0;
+
+		// TODO check if a pheromone is not already in the same position. If so,
+		// increase the pheromone strength in that position by the required
+		// value (i.e. unit).
 
 		// drop pheromone
 		Pheromone pheromone = new Pheromone(PheromoneType.Hunter);
