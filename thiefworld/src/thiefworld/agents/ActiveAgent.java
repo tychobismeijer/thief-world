b210e59f0f1002f098750a20efabb1a997e0baec
keesvangelder@gmail.com <keesvangelder@gmail.com@234c8a7c-79c9-3d8c-7b9f-80a8cb237f45>
2011-12-08 12:44:36 +0000

    Hey guys! Some real work has been done now. Here's the deal:
    Every ActiveAgent has a personalObserver object (Observer) associated to it. If  you want to call any methods regarding observing from the ActiveAgent, just call personalObserver.methodcall(parameters). 
    
    Currently the following methods are present: 
    
    - searchForNestWithin range: returns nest closest to the agent. Nest must be WITHIN RANGE. Stefan uses this method for ActiveAgent, but he could also opt for the next one (which may make things easier)
    - searchForNest: returns nest closest to the agent. Nest DOES NOT have to be within range. This can allow an agent to always find the closest nest, without conserning range. Can make returning food more easy. Not per-se unrealistic that agent can always know where the nest(s) is/are
    - getPheromonesWithinRange: returns pheromones of desired type that are within range
    - getAverageSuccessWithinRange: returns the average personal success of a task type (hunter/gatherer) within the range of the agent. This can be used to adjust internal success value(s) (was discussed with Daniel during the last meeting)
    - getClosestFoodSourceWithinRange: from all the food sources of the desired type within range, this returns the closest one. Closest might be best, but highest quantity could also be best (see next method)
    - getBestFoodSourceWithinRange: from all the food sources of the desired type within range, this returns the one with the highest quantity of food
    
    Some of these were made by Stefan in the ActiveAgent class. I've adjusted things slightly were necessary, and removed the methods from the ActiveAgent class. Everything is in the Observer class now, and all method calls that were there in ActiveAgent have been with personalObserver.methodcall(parameters) calls were necessary. 
    
    Testing is difficult, but I think it should be fine now. Let me know if there's anything wrong or feel free to adjust things yourself.

--- thiefworld/src/thiefworld/agents/ActiveAgent.java
+++ thiefworld/src/thiefworld/agents/ActiveAgent.java
@@ -10,11 +10,15 @@ import thiefworld.main.ThiefWorld;
 import thiefworld.util.Utilities;
 
 public abstract class ActiveAgent extends Agent {
+	
+	Observer personalObserver;
+	
 	public ActiveAgent() {
 		// set task switching threshold
 		switchThreshold = Utilities.nextDouble();
 		health = 1.0;
 		maxAllowedFood = ActiveAgent.getDefaultMaxCarriedFood();
+		personalObserver = new Observer(this);
 	}
 
 	private static final long serialVersionUID = 5597485865861516823L;
@@ -265,7 +269,7 @@ public abstract class ActiveAgent extends Agent {
 	 */
 	protected void returnFood(ThiefWorld world, PheromoneType pheromonesType) {
 		// search for a nearby nest
-		Nest closestNest = searchForNest(world, ActiveAgent.getAgentRange());
+		Nest closestNest = personalObserver.searchForNestWithinRange(world);
 
 		if (closestNest != null) {
 			// is the nest within drop-off distance?
@@ -306,9 +310,8 @@ public abstract class ActiveAgent extends Agent {
 			}
 		} else {
 			// search for nearby returning pheromones
-			Bag nearbyPheromones = getNearbyPheromones(world,
-					ActiveAgent.getAgentRange(), pheromonesType);
-
+			Bag nearbyPheromones = personalObserver.getPheromonesWithinRange(world, pheromonesType);
+			
 			Bag goingPheromones = new Bag();
 			Bag returningPheromones = new Bag();
 
@@ -374,38 +377,6 @@ public abstract class ActiveAgent extends Agent {
 		}
 	}
 
-	/**
-	 * Searches for a close by nest within a specified range.
-	 * 
-	 * @param world
-	 *            the {@link thiefworld.main.ThiefWorld ThiefWorld} reference.
-	 * @param range
-	 *            the distance to the agent at which the nest is searched for.
-	 * @return the closest nest around the nest or null if there is none.
-	 */
-	protected Nest searchForNest(ThiefWorld world, double range) {
-		Double2D myPosition = world.map.getObjectLocation(this);
-		Bag closebyAgents = world.map.getObjectsWithinDistance(myPosition,
-				range);
-
-		double closebyNestDistance = Double.MAX_VALUE;
-		Nest closebyNest = null;
-
-		for (int i = 0; i < closebyAgents.size(); i++) {
-			if (closebyAgents.get(i).getClass() == Nest.class) {
-				Nest nest = (Nest) closebyAgents.get(i);
-
-				Double2D nestPosition = world.map.getObjectLocation(nest);
-				if (myPosition.distance(nestPosition) < closebyNestDistance) {
-					closebyNestDistance = myPosition.distance(nestPosition);
-					closebyNest = nest;
-				}
-			}
-		}
-
-		return closebyNest;
-	}
-
 	protected void followPheromoneTrail(ThiefWorld world, Bag pheromones) {
 		MutableDouble2D pheromoneTrail = new MutableDouble2D();
 		Double2D myPosition = world.map.getObjectLocation(this);
@@ -440,24 +411,6 @@ public abstract class ActiveAgent extends Agent {
 
 	}
 
-	protected Bag getNearbyPheromones(ThiefWorld world, double range,
-			PheromoneType pheromonesType) {
-		Double2D myPosition = world.map.getObjectLocation(this);
-		Bag pheromonesCloseby = world.map.getObjectsWithinDistance(myPosition,
-				range);
-
-		Bag selectedPheromones = new Bag();
-		for (int i = 0; i < pheromonesCloseby.size(); i++) {
-			if (pheromonesCloseby.get(i).getClass() == Pheromone.class) {
-				Pheromone pheromone = (Pheromone) pheromonesCloseby.get(i);
-				if (pheromone.getType() == pheromonesType)
-					selectedPheromones.add(pheromone);
-			}
-		}
-
-		return selectedPheromones;
-	}
-
 	/**
 	 * Tries to find and extract food from food sources within the agent's
 	 * range.
@@ -468,58 +421,13 @@ public abstract class ActiveAgent extends Agent {
 	 *            the type of food which the agent searches for.
 	 */
 	protected void goAfterFood(ThiefWorld world, Class<?> foodType) {
-		FoodSource closestFoodSource = getClosestFoodSource(world, foodType);
+		FoodSource closestFoodSource = personalObserver.getClosestFoodSourceWithinRange(world, foodType);
 
 		if (closestFoodSource != null) {
 			examineFoodSource(world, closestFoodSource);
 		} else {
 			wonderAround(world);
 		}
-	}
-
-	/**
-	 * Check to see if there is a close by food source and if there are
-	 * multiple, it will choose the closest to the current agent's position.
-	 * 
-	 * @param world
-	 *            the {@link thiefworld.main.ThiefWorld ThiefWorld} reference.
-	 * @param foodSourceType
-	 *            the type of food source which you are looking for.
-	 * @return the closest food source or null if there is none in the agent's
-	 *         range.
-	 */
-	protected FoodSource getClosestFoodSource(ThiefWorld world,
-			Class<?> foodSourceType) {
-		Double2D myPosition = world.map.getObjectLocation(this);
-
-		// follow the pheromone trail to a food source
-		Bag agentsInRange = world.map.getObjectsWithinDistance(myPosition,
-				ActiveAgent.getAgentRange());
-
-		double closestFoodSourceDistance = Double.MAX_VALUE;
-		FoodSource closestFoodSource = null;
-
-		// go through all food sources of the required type
-		for (int i = 0; i < agentsInRange.size(); i++) {
-			if (agentsInRange.get(i).getClass() == foodSourceType) {
-				FoodSource foodSource = (FoodSource) agentsInRange.get(i);
-
-				// check to see if there is any food left in them
-				if (foodSource.isActive()) {
-					Double2D foodSourcePosition = world.map
-							.getObjectLocation(foodSource);
-
-					// check if it's the closest food source available
-					if (myPosition.distance(foodSourcePosition) < closestFoodSourceDistance) {
-						closestFoodSourceDistance = myPosition
-								.distance(foodSourcePosition);
-						closestFoodSource = foodSource;
-					}
-				}
-			}
-		}
-
-		return closestFoodSource;
 	}
 
 	/**
