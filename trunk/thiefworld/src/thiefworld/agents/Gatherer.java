b3ad1757719109bdff611f3b5d37bffca4f280c0
daniro@gmail.com <daniro@gmail.com@234c8a7c-79c9-3d8c-7b9f-80a8cb237f45>
2011-12-11 16:53:38 +0000

    Changed role switch policies

--- trunk/thiefworld/src/thiefworld/agents/Gatherer.java
+++ trunk/thiefworld/src/thiefworld/agents/Gatherer.java
@@ -41,6 +41,48 @@ public class Gatherer extends ActiveAgent {
 	protected void thinkAboutSwitchingJobs(ThiefWorld world) {
 		Logger log = Logger.getLogger(getName());
 		double probability = ActiveAgent.switchProb;
+		//System.out.println("initial switch probability: "+ probability);
+		
+		//Gather information about success of other agents
+		double currentHuntingSuccess = this.personalObserver.getAverageSuccessWithinRange(world, Hunter.class);
+		double currentGatheringSuccess = this.personalObserver.getAverageSuccessWithinRange(world, Gatherer.class);
+		//System.out.println("Current hunting succes: "+ currentHuntingSuccess);
+		//System.out.println("Current Gathering succes: "+ currentGatheringSuccess);
+		//Only update information if there are other agents in the neighborhood
+		if(currentHuntingSuccess != -1)
+			this.huntingSuccess += 0.7 * (currentHuntingSuccess - huntingSuccess);
+		if(currentGatheringSuccess != -1)
+			this.gatheringSuccess += 0.7 * (currentGatheringSuccess - gatheringSuccess);
+		
+		System.out.println("New value for internal hunting succes: "+ huntingSuccess);
+		System.out.println("New value for internal Gathering succes: "+ gatheringSuccess);
+		System.out.println("personal succes: "+ personalSuccess);
+		//Check whether it is worth it to switch
+		/*if agent performs less than average on hunting and gathering is more successful than hunting
+		 * add the difference in performance to the switch probability
+		*/
+		if(this.personalSuccess <= this.gatheringSuccess && this.gatheringSuccess < this.huntingSuccess){
+			probability += weight * (huntingSuccess - gatheringSuccess);
+		}
+		
+		//System.out.println("new switch probability: "+ probability);
+		double threshold = 0.05 + Utilities.nextDouble(-0.05, 0.05);//this.switchThreshold + Utilities.nextDouble(-0.05, 0.05);
+		System.out.println("switch depends on:\n "+ (probability - gatheringSkill) + "being larger than "
+							+ threshold);
+		
+		// Regardless of performance, there is always a probability of switching
+		if(probability - 0.5*this.gatheringSkill > (threshold) ) {
+			System.out.println("SWITCHING");
+			log.log(Level.INFO,
+					this.getName() + " is switching from Gatherer to Hunter");
+			this.replace(world, Hunter.class);
+		}
+		
+		
+		/*
+		 *  Logger log = Logger.getLogger(getName());
+
+		double probability = ActiveAgent.switchProb;
 		
 		//Gather information about success of other agents
 		double currentHuntingSuccess = this.personalObserver.getAverageSuccessWithinRange(world, Hunter.class);
@@ -55,7 +97,7 @@ public class Gatherer extends ActiveAgent {
 		//Check whether it is worth it to switch
 		/*if agent performs less than average on hunting and gathering is more successful than hunting
 		 * add the difference in performance to the switch probability
-		*/
+		//
 		if(this.personalSuccess < this.gatheringSuccess && this.gatheringSuccess < this.huntingSuccess){
 				probability += weight * (this.gatheringSuccess - this.personalSuccess);
 				System.out.println("The conditions are right for switching..");
@@ -68,5 +110,6 @@ public class Gatherer extends ActiveAgent {
 			System.out.println("The agent is actually switching!!!");
 			this.replace(world, Hunter.class);
 		}
+		*/
 	}
 }
--- trunk/thiefworld/src/thiefworld/agents/Hunter.java
+++ trunk/thiefworld/src/thiefworld/agents/Hunter.java
@@ -45,27 +45,38 @@ public class Hunter extends ActiveAgent {
 	protected void thinkAboutSwitchingJobs(ThiefWorld world) {
 		Logger log = Logger.getLogger(getName());
 		double probability = ActiveAgent.switchProb;
+		//System.out.println("initial switch probability: "+ probability);
 		
 		//Gather information about success of other agents
 		double currentHuntingSuccess = this.personalObserver.getAverageSuccessWithinRange(world, Hunter.class);
 		double currentGatheringSuccess = this.personalObserver.getAverageSuccessWithinRange(world, Gatherer.class);
-		
+		//System.out.println("Current hunting succes: "+ currentHuntingSuccess);
+		//System.out.println("Current Gathering succes: "+ currentGatheringSuccess);
 		//Only update information if there are other agents in the neighborhood
 		if(currentHuntingSuccess != -1)
-			this.huntingSuccess += 0.5 * (currentHuntingSuccess - huntingSuccess);
+			this.huntingSuccess += 0.7 * (currentHuntingSuccess - huntingSuccess);
 		if(currentGatheringSuccess != -1)
-			this.gatheringSuccess += 0.5 * (currentGatheringSuccess - gatheringSuccess);
+			this.gatheringSuccess += 0.7 * (currentGatheringSuccess - gatheringSuccess);
 		
+		System.out.println("New value for internal hunting succes: "+ huntingSuccess);
+		System.out.println("New value for internal Gathering succes: "+ gatheringSuccess);
+		System.out.println("personal succes: "+ personalSuccess);
 		//Check whether it is worth it to switch
 		/*if agent performs less than average on hunting and gathering is more successful than hunting
 		 * add the difference in performance to the switch probability
 		*/
-		if(this.personalSuccess < this.huntingSuccess && this.gatheringSuccess > this.huntingSuccess){
-			probability += weight * (huntingSuccess - this.personalSuccess);
+		if(this.personalSuccess <= this.huntingSuccess && this.gatheringSuccess > this.huntingSuccess){
+			probability += weight * (gatheringSuccess - huntingSuccess);
 		}
 		
+		//System.out.println("new switch probability: "+ probability);
+		double threshold = 0.05 + Utilities.nextDouble(-0.05, 0.05);//this.switchThreshold + Utilities.nextDouble(-0.05, 0.05);
+		System.out.println("switch depends on:\n "+ (probability - huntingSkill) + "being larger than "
+							+ threshold);
+		
 		// Regardless of performance, there is always a probability of switching
-		if(probability - this.huntingSkill > (this.switchThreshold + Utilities.nextDouble(-0.1, 0.1)) ) {
+		if(probability - 0.1*this.huntingSkill > (threshold) ) {
+			System.out.println("SWITCHING");
 			log.log(Level.INFO,
 					this.getName() + " is switching from Hunter to Gatherer");
 			this.replace(world, Gatherer.class);
