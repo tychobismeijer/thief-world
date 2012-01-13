8838ba63c5e1dd96c020b225292b34598025518e
tychobismeijer@gmail.com <tychobismeijer@gmail.com@234c8a7c-79c9-3d8c-7b9f-80a8cb237f45>
2012-01-13 20:53:25 +0000

    Added experiment 2

--- trunk/aso/results/experiment2/generate.m
+++ trunk/aso/results/experiment2/generate.m
@@ -4,10 +4,17 @@ max_gap_size = max(data(:,1));
 p = zeros(max_gap_size+1, 1);
 for gap_size = 0:max_gap_size
     d = data(data(:,1)==gap_size,:);
-    d = d(d(:,4) ~= 0, :)
-    p(gap_size+1) = mean(d(:,4))
+    d = d(d(:,4) ~= 0, :);
+    p(gap_size+1) = mean(d(:,4));
 end
-plot(0:max_gap_size, p);
+plot(0:max_gap_size, p,
+    'linewidth', 2,
+    'color', 'black');
+axis([0, max_gap_size, 0, max(p)])
 xlabel('Gap Size');
 ylabel('Average number of reconfigurations');
-print(['plot' num2str(gap_size) '.pdf'], '-dpdf');
+print('plot.pdf',
+    '-dpdfwrite',
+    '-S200,200',
+    '-tight',
+    '-FLMRoman10:16');
